0e7a6f160ecc1c9265dd20a4175d3c6abbb2dc09
Jana Sebestova <lizarde@seznam.cz>
2021-06-04 16:01:32 +0200

    za me hotovo

--- src/Jana/index.jsx
+++ src/Jana/index.jsx
@@ -8,23 +8,23 @@ const Jana = () => {
     </div>
     <div className="obsah">
       <div className="pics">
-        <img className="rules__img" id="pic1" src="../assets/map_detail.png" alt="" />
-        <img className="rules__img" id="pic2" src="../assets/rebus.jpeg" alt="" />
-        <img className="rules__img" id="pic3" src="../assets/watch_col.png" alt="" />
+        <img className="rules__img" id="pic1" src="../assets/jpic1.jpg" alt="" />
+        <img className="rules__img" id="pic2" src="../assets/profilovka_small.jpg" alt="" />
+        <img className="rules__img" id="pic3" src="../assets/jpic3.jpg" alt="" />
       </div>
       <div className="text">
         <h3>Vedoucí kreativního oddělení</h3>
         <p>
-          Odstavec 1
+          Nu kde začít... Momentálně nevím, kde mi hlava stojí – projekt v Digitální Akademii běží na plné obrátky, do toho práce, tři děti a achievy v PoEčku.. 
         </p>
         <p>
-          Odstavec 2
+          Takže popořadě. Jsem maminka třech úžasně zlobivých dětí – Silvík (6), Dodo (4) a Verda (2). Takže se kolem mě pořád něco děje. Covid situace nám docela omezila naše oblíbené aktivity, jakožto návštěva zoo, zajít si na zmrzku, chodit plavat, ... Tak jsme ten poslední rok nějak přežili spíše než si užili. Ale taky jsme se naučily nosit náhubky, díky kterým děti nekousají do kolemjdoucích (ne že by to před tím dělaly...).
         </p>
         <p>
-          Odstavec 3
+          K dětem pracuji na poloviční úvazek v Honeywellu na finančním oddělení a už se těším, jak v průběhu dvou let změním kareiéru a vrhnu se do světa programování. Jednou už jsem „kariéru“ měnila, když jsem po střední strojárně nastoupila na VŠ ekonomickou. Změna je život.
         </p>
         <p>
-          Odstavec 4
+        I s dětičkami mi ještě pár koníčků zbylo a to: každý týden chodíme na Hospodské kvízy (v covidu hrajeme aspoň domácí verzi), sport (zejména turistika a brusle) a hry (deskovky, když je společnost, jinak hlavně PoE, TFT a vlastnoručně naprogramované piškvorky). 
         </p>
       </div>
     </div>
